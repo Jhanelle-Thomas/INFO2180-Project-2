55cd699fb72931663ab0acf287daa060b4d98f12
Jhanelle-Thomas <jhanelle.thomas@yahoo.com>
2016-10-30 12:42:59 -0400

    Tiles move to the empty space when clicked

--- fifteen.js
+++ fifteen.js
@@ -1,13 +1,22 @@
 $(document).ready(function() {
-	var puzzlearea = $("#puzzlearea")
+	var shuff = false;
+	$("#puzzlearea div").addClass("puzzlepiece");
 	var pieces = document.getElementsByClassName("puzzlepiece");
 	var shufflebutton = document.getElementById("shufflebutton")
+	
+	//Array which provides relevant positions for puzzlepieces	
 	var positions = [["0px", "0px"],["0px", "100px"],["0px","200px"],["0px", "300px"],
 					 ["100px","0px"],["100px","100px"],["100px","200px"],["100px","300px"],
 					 ["200px","0px"],["200px","100px"],["200px","200px"],["200px","300px"],
 					 ["300px","0px"],["300px","100px"],["300px","200px"]];
+					 
+	//Position of the empty tile (Always starts in the bottom right corner)
+	var empty = [300, 300];
 	
-	
+	/*
+	Assigns each puzzlepiece to a position based on coordinates given by the 
+	positions array
+	*/
 	function order() {
 		for(var x = 0; x < pieces.length; x++) {
 			pieces[x].style.left = positions[x][1];
@@ -15,6 +24,9 @@ $(document).ready(function() {
 		}
 	}
 	
+	/* 
+	Sets the background image to the correct position for each tile
+	*/
 	function setBackground() {
 		for(var v = 0; v < pieces.length; v++) {
 			pieces[v].style.backgroundSize = "400px 400px";
@@ -24,6 +36,9 @@ $(document).ready(function() {
 		}
 	}
 	
+	/*
+	Randomly shuffles the position each tile should take
+	*/
 	function shuffle(array) {
 		for (var i = array.length - 1; i > 0; i--) {
 			var j = Math.floor(Math.random() * (i + 1));
@@ -34,12 +49,33 @@ $(document).ready(function() {
 		return array;
 	}
 	
+	/*
+	Randomly reorganizes the location of the puzzle pieces when the button 
+	is pressed
+	*/
 	shufflebutton.addEventListener("click", function() {
 		positions = shuffle(positions);
 		order();
+		shuff = true;
 	});
 	
-	$("#puzzlearea div").addClass("puzzlepiece");	
+	/*
+	Sets up the board, putting puzzle pieces and background images in the 
+	correct positions
+	*/
 	order();
 	setBackground();	
+	
+	for(var v = 0; v < pieces.length; v++) {
+		pieces[v].addEventListener("click", function() {
+			if (shuff) {
+				var temp = [this.offsetTop, this.offsetLeft]
+				this.style.left = empty[1] + "px";
+				this.style.top = empty[0] + "px";
+				empty = temp;
+			}
+		});
+	}
+	
+	
 });
